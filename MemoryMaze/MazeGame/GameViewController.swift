84d5b9ef498a93507fa3eb9e28c15a544d881c5a
Garry Fanata <garryfanata@gmail.com>
2019-06-20 15:49:37 +0700

    MVP Add Game Logic

--- MemoryMaze/MazeGame/GameViewController.swift
+++ MemoryMaze/MazeGame/GameViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class GameViewController: UIViewController {
     
     var currentLevel : Int = -1
+    var moveFlag = true
 
     @IBOutlet var gameLevelLabel: UILabel!
     @IBOutlet var mazeScreen: UIView!
@@ -39,16 +40,16 @@ class GameViewController: UIViewController {
         self.drawMaze()
         self.drawEndPoints()
         
+        let inc = Double(mazeScreen.frame.height/10)
         blockImage = UIImageView(image: CustomizationViewController.selected.image)
-        blockImage.frame = CGRect(x: 0, y: 0, width: mazeScreen.frame.height/10, height: mazeScreen.frame.height/10)
+        blockImage.frame = CGRect(x: round(1000.0 * (Double(endPoints[0].0) * inc)) / 1000.0, y: round(1000.0 * (Double(endPoints[0].1) * inc)) / 1000.0, width: inc, height: inc)
         print(blockImage.frame.height)
         print("blockImage frame origin x = \(blockImage.frame.origin.x)")
         print("blockImage frame origin y = \(blockImage.frame.origin.y)")
         
         mazeScreen.addSubview(blockImage)
         
-        upButton.frame.size = CGSize(width: 25, height: 25)
-        upButton.setImage(UIImage(named: "U_Arrow_0"), for: UIControl.State.normal)
+        upButton.setImage(UIImage(named: "U_Arrow_0"), for: .normal)
         upButton.contentMode = .center
         upButton.autoresizesSubviews = true
         upButton.imageRect(forContentRect: CGRect(x: 0, y: 0, width: 25, height: 25))
@@ -77,54 +78,65 @@ class GameViewController: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        self.resetMaze()
+    }
+    
     @IBAction func upButtonPressed(_ sender: Any) {
         print("up")
-        if (blockImage.frame.origin.y >= blockImage.frame.height - 0.1){
+        if (blockImage.frame.origin.y >= blockImage.frame.height - 0.1 && moveFlag){
             blockImage.frame.origin.y -= blockImage.frame.height
         } else {
             print("WALLED")
         }
-//        print("blockImage frame origin x = \(blockImage.frame.origin.x)")
-//        print("blockImage frame origin y = \(blockImage.frame.origin.y)")
+        print("blockImage frame origin x = \(round(blockImage.frame.origin.x/blockImage.frame.width))")
+        print("blockImage frame origin y = \(round(blockImage.frame.origin.y/blockImage.frame.width))")
+        checkCollision(x: Double(round(blockImage.frame.origin.x/blockImage.frame.width)), y: Double(round(blockImage.frame.origin.y/blockImage.frame.width)))
 //        print("blockImage frame height = \(blockImage.frame.height)")
     }
     
     @IBAction func rightButtonPressed(_ sender: Any) {
         print("right")
-        if (blockImage.frame.origin.x <= mazeScreen.frame.width - blockImage.frame.width-0.1){
+        if (blockImage.frame.origin.x <= mazeScreen.frame.width - blockImage.frame.width-0.1 && moveFlag){
             blockImage.frame.origin.x += blockImage.frame.width
         } else {
             print("WALLED")
         }
-//        print("blockImage frame origin x = \(blockImage.frame.origin.x)")
-//        print("blockImage frame origin y = \(blockImage.frame.origin.y)")
+        print("blockImage frame origin x = \(round(blockImage.frame.origin.x/blockImage.frame.width))")
+        print("blockImage frame origin y = \(round(blockImage.frame.origin.y/blockImage.frame.width))")
+        checkCollision(x: Double(round(blockImage.frame.origin.x/blockImage.frame.width)), y: Double(round(blockImage.frame.origin.y/blockImage.frame.width)))
 //        print("mazeScreen frame width = \(mazeScreen.frame.width)")
 //        print("blockImage frame width = \(blockImage.frame.height)")
     }
     
     @IBAction func downButtonPressed(_ sender: Any) {
         print("down")
-        if (blockImage.frame.origin.y <= mazeScreen.frame.height - blockImage.frame.height-0.1){
+        if (blockImage.frame.origin.y <= mazeScreen.frame.height - blockImage.frame.height-0.1 && moveFlag){
             blockImage.frame.origin.y += blockImage.frame.height
         } else {
             print("WALLED")
         }
-//        print("blockImage frame origin x = \(blockImage.frame.origin.x)")
-//        print("blockImage frame origin y = \(blockImage.frame.origin.y)")
+        
+        print("blockImage frame origin x = \(round(blockImage.frame.origin.x/blockImage.frame.width))")
+        print("blockImage frame origin y = \(round(blockImage.frame.origin.y/blockImage.frame.width))")
+        checkCollision(x: Double(round(blockImage.frame.origin.x/blockImage.frame.width)), y: Double(round(blockImage.frame.origin.y/blockImage.frame.width)))
+        
     }
     
     @IBAction func leftButtonPressed(_ sender: Any) {
         print("left")
-        if (blockImage.frame.origin.x >= blockImage.frame.width - 0.1){
+        if (blockImage.frame.origin.x >= blockImage.frame.width - 0.1 && moveFlag) {
             blockImage.frame.origin.x -= blockImage.frame.width
         } else {
             print("WALLED")
         }
-//        print("blockImage frame origin x = \(blockImage.frame.origin.x)")
-//        print("blockImage frame origin y = \(blockImage.frame.origin.y)")
+        print("blockImage frame origin x = \(round(blockImage.frame.origin.x/blockImage.frame.width))")
+        print("blockImage frame origin y = \(round(blockImage.frame.origin.y/blockImage.frame.width))")
+        checkCollision(x: Double(round(blockImage.frame.origin.x/blockImage.frame.width)), y: Double(round(blockImage.frame.origin.y/blockImage.frame.width)))
     }
     
     func drawMaze(){
+        moveFlag = false
         var mazeImage = UIImageView(image: UIImage(named: "MazeYellow"))
         let inc = Double(mazeScreen.frame.height/10)
         
@@ -141,12 +153,81 @@ class GameViewController: UIViewController {
 
     func drawEndPoints(){
         let inc = Double(mazeScreen.frame.height/10)
-        let startPoint = UIImageView(image: UIImage(named: "Red"))
+        let startPoint = UIImageView(image: UIImage(named: "StartYellow"))
         startPoint.frame = CGRect(x: round(1000.0 * (Double(endPoints[0].0) * inc)) / 1000.0, y: round(1000.0 * (Double(endPoints[0].1) * inc)) / 1000.0, width: inc, height: inc)
-        let endPoint = UIImageView(image: UIImage(named: "Green"))
+        let endPoint = UIImageView(image: UIImage(named: "FlagYellow"))
         endPoint.frame = CGRect(x: round(1000.0 * (Double(endPoints[1].0) * inc)) / 1000.0, y: round(1000.0 * (Double(endPoints[1].1) * inc)) / 1000.0, width: inc, height: inc)
         mazeScreen.addSubview(startPoint)
         mazeScreen.addSubview(endPoint)
+    }
+    
+    @discardableResult func checkCollision(x : Double, y : Double) -> Bool{
+        let currentCoord = (Int(x),Int(y))
+        
+        if currentCoord == endPoints[1]{
+                print("WINNERRR")
+            let alert = UIAlertController(title: "Congratulations, You Won!", message: "You have completed this level", preferredStyle: UIAlertController.Style.alert)
+            
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "Back To Level Selection", style: UIAlertAction.Style.default, handler: { action in
+                
+                // do something like...
+                print("Won")
+//                self.dismiss(animated: true, completion: nil)
+                self.navigationController?.popViewController(animated: true)
+                
+            }))
+            
+            
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            print("ALERT")
+            
+            return true
+        }
+
+        for coords in mazecoords {
+            if currentCoord == coords{
+                print("LOSERRR")
+                
+                for walls in self.maze{
+                    walls.isHidden = false
+                    self.mazeScreen.addSubview(walls)
+                }
+                moveFlag = false
+                
+                let alert = UIAlertController(title: "You Lost!", message: "You have failed", preferredStyle: UIAlertController.Style.alert)
+                
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "Try Again", style: UIAlertAction.Style.destructive, handler: { action in
+                    
+                    // do something like...
+                    print("RESET")
+                    self.resetMaze()
+                    
+                }))
+                
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+                return true
+            }
+        }
+        return false
+    }
+    
+    func resetMaze(){
+        let inc = Double(self.mazeScreen.frame.height/10)
+        self.blockImage.frame.origin.x = CGFloat(round(1000.0 * (Double(self.endPoints[0].0) * inc))/1000)
+        self.blockImage.frame.origin.y = CGFloat(round(1000.0 * (Double(self.endPoints[0].1) * inc))/1000)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            print("Bye now")
+            for walls in self.maze{
+                walls.isHidden = true
+                self.mazeScreen.addSubview(walls)
+            }
+            self.moveFlag = true
+        }
     }
 }
 
